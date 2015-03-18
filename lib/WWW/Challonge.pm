0ab13620d366c957bda5a617f89c5989a619e7b1
Alex Kerr <kirbyman62@gmail.com>
2015-03-18 14:07:08 +0000

    Documented WWW::Challonge

--- lib/WWW/Challonge.pm
+++ lib/WWW/Challonge.pm
@@ -6,7 +6,7 @@ use warnings;
 
 =head1 NAME
 
-WWW::Challonge - The great new WWW::Challonge!
+WWW::Challonge - Perl wrapper for the Challonge API
 
 =head1 VERSION
 
@@ -16,37 +16,125 @@ Version 0.01
 
 our $VERSION = '0.01';
 
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+Access the Challonge API within Perl. Contains all the functions within the API,
+as documented L<here|http:://api.challonge.com/v1>.
 
 Perhaps a little code snippet.
 
     use WWW::Challonge;
 
-    my $foo = WWW::Challonge->new();
+    my $c = WWW::Challonge->new({
+		username => $username,
+		api_key => $key
+	});
     ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
 
 =head1 SUBROUTINES/METHODS
 
-=head2 function1
+=head2 new
+
+Creates a new C<WWW::Challonge> object. Takes in a username and api key, both
+of which are required:
+
+    my $c = WWW::Challonge->new({
+		username => $username,
+		api_key => $key
+	});
 
 =cut
 
-sub function1 {
+sub new
+{
 }
 
-=head2 function2
+=head2 index
+
+Returns an arrayref of all C<WWW::Challonge::Tournament> objects owned by the
+user authenticated with in the 'new' request (the logged in user, so to speak).
+Takes a number of optional arguments: 
+
+=over 4
+
+=item state
+
+Get tournaments based on their progress:
+
+=over 4
+
+=item all
+
+Gets all tournaments regardless of state.
+
+=item pending
+
+Gets all tournaments that have yet to start.
+
+=item in_progress
+
+Gets all tournaments that have started but have not finished.
+
+=item ended
+
+Gets all tournaments that have finished.
+
+=back
+
+=item type
+
+Gets all tournaments of the given type:
+
+=over 4
+
+=item single_elimination
+
+=item double_elimination
+
+=item round_robin
+
+=item swiss
+
+=back
+
+=item created_after
+
+Gets all the tournaments created after the given date. Can be given as a string
+(YYYY-MM-DD) or as a C<DateTime> object.
+
+=item created_before
+
+Gets all the tournaments created before the given date. Can be given as a string
+(YYYY-MM-DD) or as a C<DateTime> object.
+
+=item subdomain
+
+Gets all tournaments created under the given subdomian.
+
+=back
+
+	my $tournies  = $c->index();
+	my $tournies2 = $c->index({
+		type => "double_elimination",
+		created_after => "2015-03-18",
+	});
 
 =cut
 
-sub function2 {
+sub index
+{
+}
+
+=head2 show
+
+Gets a single C<WWW::Challonge::Tournament> object by the given id or URL:
+
+	my $tourney = $c->show("sample_tournament_1");
+
+=cut
+
+sub show
+{
 }
 
 =head1 AUTHOR
@@ -59,15 +147,11 @@ Please report any bugs or feature requests to C<bug-www-challonge at rt.cpan.org
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-Challonge>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
 
-
-
-
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
     perldoc WWW::Challonge
-
 
 You can also look for information at:
 
@@ -91,9 +175,22 @@ L<http://search.cpan.org/dist/WWW-Challonge/>
 
 =back
 
+=head1 SEE ALSO
+
+=over 4
+
+=item L<WWW::Challonge::Tournament>
+
+=item L<WWW::Challonge::Participant>
+
+=item L<WWW::Challonge::Match>
+
+=back
 
 =head1 ACKNOWLEDGEMENTS
 
+Everyone on the L<Challonge|http://challonge.com> team for making such a great
+service.
 
 =head1 LICENSE AND COPYRIGHT
 
@@ -134,7 +231,6 @@ YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
 CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
 CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 
 =cut
 
